ef23848fe1261284fb6163502ac685dd65c8c5f6
Brendan Hendrata <bhendrata@cmu.edu>
2026-01-04 02:49:40 -0500

    implemented regfile header

--- rtl/registers.sv
+++ rtl/registers.sv
@@ -1,2 +1,24 @@
-module registers ();
+// ## Registers
+// 32 general purpose 32-bit registers (R0-R31). R0 is hardwired to zero.
+
+// Inputs:
+// - Register select (5-bits) x 3 (which to read/write) (rd, rs1, rs2)
+// - Register write enable (1-bit)
+// - Register write data (32-bits)
+// - Clock, Reset
+
+// Outputs:
+// - Register read data (32-bits) x 2
+
+module registers #(
+    parameter int WIDTH = 32,
+)
+(
+    input  logic [4:0] sel_rd, sel_rs1, sel_rs2,
+    input  logic wr_en,
+    input  logic [WIDTH-1:0] wr_data,
+    input  logic clock, reset,
+
+    output logic [WIDTH-1:0] rs1_data, rs2_data
+);
 endmodule: registers
